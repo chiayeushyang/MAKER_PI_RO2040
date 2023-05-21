d1d866ce42b7995f54a0dab09fde5b105551303f
Chia Yeu Shyang <yeushyang020825@gmail.com>
2023-05-21 22:39:39 +0800

    first commit

---
+++ ADS1115/ADS1115.ino
@@ -0,0 +1,63 @@
+//
+//    FILE: ADS_RP2040_WIRE1.ino
+//  AUTHOR: Rob.Tillaart / Intubun
+// PURPOSE: read analog input
+//     URL: https://github.com/RobTillaart/ADS1X15
+
+// test
+// connect 1 potmeter
+//
+// GND ---[   x   ]------ 5V
+//            |
+//
+// measure at x (connect to AIN0).
+
+
+#include "ADS1X15.h"
+
+
+// choose your sensor
+// ADS1013 ADS(0x48);
+// ADS1014 ADS(0x48);
+// ADS1015 ADS(0x48);
+// ADS1113 ADS(0x48);
+// ADS1114 ADS(0x48);
+ADS1115 ADS(0x48, &Wire);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("ADS1X15_LIB_VERSION: ");
+  Serial.println(ADS1X15_LIB_VERSION);
+
+  ADS.begin(0, 1);   // SDA (Pin 26), SCL(Pin 27)
+  ADS.setDataRate(7);  // 0 = slow   4 = medium   7 = fast
+  ADS.setMode(0);      // continuous mode
+}
+
+
+void loop()
+{
+  ADS.setGain(1);
+ 
+  int16_t val_0 = ADS.readADC(0);  
+  int16_t val_1 = ADS.readADC(1);  
+  int16_t val_2 = ADS.readADC(2);  
+  int16_t val_3 = ADS.readADC(3);  
+ 
+  float f = ADS.toVoltage(2);  // voltage factor
+ 
+  Serial.print("\tAnalog0: "); Serial.print(val_0); Serial.print('\t'); Serial.println(val_0 * f, 3);
+  Serial.print("\tAnalog1: "); Serial.print(val_1); Serial.print('\t'); Serial.println(val_1 * f, 3);
+  Serial.print("\tAnalog2: "); Serial.print(val_2); Serial.print('\t'); Serial.println(val_2 * f, 3);
+  Serial.print("\tAnalog3: "); Serial.print(val_3); Serial.print('\t'); Serial.println(val_3 * f, 3);
+  Serial.println();
+ 
+  delay(500);
+}
+
+
+
+// -- END OF FILE --
