5281d498ac03639ffd3527d35336984fea2e3e01
Chia Yeu Shyang <yeushyang020825@gmail.com>
2023-05-22 23:33:29 +0800

    try neopixle

--- HC-05/HC-05.ino
+++ HC-05/HC-05.ino
@@ -4,6 +4,12 @@
 //RX(5) -> HC-05 TX 
 //TX(4) -> HC-05 RX 
 
+int Button = 20; 
+
+int buttonState = 0;   
+
+boolean buttonFlag = true;
+
 void setup() {
 
   Serial.begin(9600);
@@ -12,14 +18,30 @@ void setup() {
   Serial2.setTX(4);
 
   Serial2.begin(9600);
+
+  pinMode(Button, INPUT_PULLUP);
 }
 
 void loop() {
+  buttonState = digitalRead(Button);
+  Serial.println(buttonState);
+
+  delay(500);
   if (Serial.available()) {
     Serial2.write(Serial.read());
   }
 
   if (Serial2.available()) {
     Serial.write(Serial2.read());
+  }
+
+  if (buttonState == HIGH && buttonFlag == true) {
+    // turn LED on:
+
+     buttonFlag = true;
+  } else {
+    // turn LED off:
+
+    buttonFlag = false;
   }
 }
---
+++ NEOPIXLE/NEOPIXLE.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 18
+#define NUM_LEDS 2
+
+Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+  pixels.show(); // Initialize all pixels to off
+}
+
+void loop() {
+  // Example: Fill the strip with red color
+  for (int i = 0; i < NUM_LEDS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0)); // Set pixel color to red (RGB)
+    
+  }
+pixels.show(); // Update the strip
+    delay(100); // Delay between pixels
+  // Example: Turn off all LEDs
+  pixels.clear();
+  pixels.show();
+  delay(2000); // Delay before repeating the loop
+}
